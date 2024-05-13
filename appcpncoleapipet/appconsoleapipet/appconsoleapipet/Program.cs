ca54220dde3f70b3ac9c99faf258903cc31be7cb
danfg11 <andreesdiiaz19@gmail.com>
2024-05-13 08:07:54 +0200

    desarrollo peticion y respuesta apipet

---
+++ appcpncoleapipet/appconsoleapipet/appconsoleapipet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+string apiUrl = "https://petstore.swagger.io/v2/pet";
+
+try
+{
+    using (HttpClient client = new HttpClient())
+    {
+        // Crear el objeto JSON con los datos de la mascota
+        string jsonData = @"{
+            ""id"": 0,
+            ""name"": ""dog""
+        }";
+
+        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+        HttpResponseMessage response = await client.PostAsync(apiUrl, new StringContent(jsonData, Encoding.UTF8, "application/json"));
+
+        Console.WriteLine($"Código de respuesta: {(int)response.StatusCode} {response.ReasonPhrase}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocurrió un error: {ex.Message}");
+}
